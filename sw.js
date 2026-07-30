353ba69e06320f08cbe707d5f13a1e0a1cbb4dbf
jorgeantonio19-sketch <jorgeantonio19@gmail.com>
2026-07-30 23:05:08 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'folha-ponto-v2';
+const ARQUIVOS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/classes.js',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARQUIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
